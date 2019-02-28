520158558df0a7a146e25e120ccaf920a81bd8c4
Frank Pedroza <f.pedroza@elsevier.com>
2019-02-28 10:36:38 -0700

    add debug logging support for tests; new test for get all books; minor test tweaks;

--- app/routes/book.js
+++ app/routes/book.js
@@ -9,8 +9,8 @@ function getBooks(req, res) {
 	let query = Book.find({});
 	query.exec((err, books) => {
 		if(err) res.send(err);
-		//If no errors, send them back to the client
-		res.json(books);
+            //If no errors, send them back to the client
+    		res.json(books);
 	});
 }
 
@@ -21,12 +21,12 @@ function postBook(req, res) {
 	//Creates a new book
 	var newBook = new Book(req.body);
 	//Save it into the DB.
-	newBook.save((err,book) => {
+	newBook.save((err, book) => {
 		if(err) {
 			res.send(err);
 		}
 		else { //If no errors, send it back to the client
-			res.json({message: "Book successfully added!", book });
+    		res.json({message: "Book successfully added!", book });
 		}
 	});
 }
@@ -37,8 +37,8 @@ function postBook(req, res) {
 function getBook(req, res) {
 	Book.findById(req.params.id, (err, book) => {
 		if(err) res.send(err);
-		//If no errors, send it back to the client
-		res.json(book);
+            //If no errors, send it back to the client
+    		res.json(book);
 	});		
 }
 
@@ -57,12 +57,20 @@ function deleteBook(req, res) {
 function updateBook(req, res) {
 	Book.findById({_id: req.params.id}, (err, book) => {
 		if(err) res.send(err);
-		Object.assign(book, req.body).save((err, book) => {
+		
+        Object.assign(book, req.body).save((err, book) => {
 			if(err) res.send(err);
-			res.json({ message: 'Book updated!', book });
+			else res.json({ message: 'Book updated!', book });
 		});	
 	});
 }
 
 //export all the functions
-module.exports = { getBooks, postBook, getBook, deleteBook, updateBook };
+module.exports = { 
+    getBooks, 
+    postBook, 
+    getBook, 
+    deleteBook, 
+    updateBook
+};
+
--- test/book.js
+++ test/book.js
@@ -8,10 +8,16 @@ let Book = require('../app/models/book');
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../server');
+let util = require('util');
+
 let should = chai.should();
 
 
 chai.use(chaiHttp);
+
+function logResponse(res) {
+    console.log("response: status=" + res.status + "\nbody=" + util.inspect(res.body, {depth: null}));
+}
 
 //Our parent block
 describe('Books', () => {
@@ -25,124 +31,186 @@ describe('Books', () => {
   * Test the /GET route
   */
   describe('/GET book', () => {
-	  it('it should GET all the books', (done) => {
-			chai.request(server)
-		    .get('/book')
-		    .end((err, res) => {
-			  	res.should.have.status(200);
-			  	res.body.should.be.a('array');
-			  	res.body.length.should.be.eql(0);
-		      done();
-		    });
-	  });
+      it('GET all the books; currently none', (done) => {
+        chai.request(server)
+            .get('/book')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.an('array');
+                res.body.length.should.be.eql(0);
+              done();
+            });
+      });
   });
+
+  /*
+  * Test the /GET route
+  */
+  describe('/GET book', () => {
+      it('GET all the books; there are some', (done) => {
+        new Book({ 
+            title: "The Lord of the Rings", 
+            author: "J.R.R. Tolkien", 
+            year: 1954, 
+            pages: 1170 
+        }).save((err, book) => {
+            if(err) assert.fail("save book failed");
+        });
+        new Book({ 
+            title: "The Bible", 
+            author: "God", 
+            year: 300, 
+            pages: 2000 
+        }).save((err, book) => {
+            if(err) assert.fail("save book failed");
+        });
+
+        chai.request(server)
+            .get('/book')
+            .end((err, res) => {
+                // logResponse(res);
+                res.should.have.status(200);
+                res.body.should.be.an('array');
+                res.body.length.should.be.eql(2);
+              done();
+            });
+      });
+  });
+
  /*
   * Test the /POST route
   */
   describe('/POST book', () => {
-	  it('it should not POST a book without pages field', (done) => {
-	  	let book = {
-	  		title: "The Lord of the Rings",
-	  		author: "J.R.R. Tolkien",
-	  		year: 1954
-	  	}
-			chai.request(server)
-		    .post('/book')
-		    .send(book)
-		    .end((err, res) => {
-			  	res.should.have.status(200);
-			  	res.body.should.be.a('object');
-			  	res.body.should.have.property('errors');
-			  	res.body.errors.should.have.property('pages');
-			  	res.body.errors.pages.should.have.property('kind').eql('required');
-		      done();
-		    });
-	  });
-	  it('it should POST a book ', (done) => {
-	  	let book = {
-	  		title: "The Lord of the Rings",
-	  		author: "J.R.R. Tolkien",
-	  		year: 1954,
-	  		pages: 1170
-	  	}
-			chai.request(server)
-		    .post('/book')
-		    .send(book)
-		    .end((err, res) => {
-			  	res.should.have.status(200);
-			  	res.body.should.be.a('object');
-			  	res.body.should.have.property('message').eql('Book successfully added!');
-			  	res.body.book.should.have.property('title');
-			  	res.body.book.should.have.property('author');
-			  	res.body.book.should.have.property('pages');
-			  	res.body.book.should.have.property('year');
-		      done();
-		    });
-	  });
+      // missing pages
+      let book = {
+        title: "The Lord of the Rings",
+        author: "J.R.R. Tolkien",
+        year: 1954
+      };
+
+      it('POST a book without pages field', (done) => {        
+        chai.request(server)
+            .post('/book')
+            .send(book)
+            .end((err, res) => {
+                // logResponse(res);
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('errors');
+                res.body.errors.should.have.property('pages');
+                res.body.errors.pages.should.have.property('kind').eql('required');
+                done();
+            });
+      });
+
+      it('POST a book with pages', (done) => {
+        book.pages = 1170;
+        
+        chai.request(server)
+            .post('/book')
+            .send(book)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.an('object');
+                res.body.should.have.property('message').eql('Book successfully added!');
+                res.body.book.should.have.property('title');
+                res.body.book.should.have.property('author');
+                res.body.book.should.have.property('pages');
+                res.body.book.should.have.property('year');
+                done();
+            });
+      });
   });
+
  /*
   * Test the /GET/:id route
   */
   describe('/GET/:id book', () => {
-	  it('it should GET a book by the given id', (done) => {
-	  	let book = new Book({ title: "The Lord of the Rings", author: "J.R.R. Tolkien", year: 1954, pages: 1170 });
-	  	book.save((err, book) => {
-	  		chai.request(server)
-		    .get('/book/' + book.id)
-		    .send(book)
-		    .end((err, res) => {
-			  	res.should.have.status(200);
-			  	res.body.should.be.a('object');
-			  	res.body.should.have.property('title');
-			  	res.body.should.have.property('author');
-			  	res.body.should.have.property('pages');
-			  	res.body.should.have.property('year');
-			  	res.body.should.have.property('_id').eql(book.id);
-		      done();
-		    });
-	  	});
-			
-	  });
+      it('GET a book by the given id', (done) => {
+        let book = new Book({ 
+            title: "The Lord of the Rings", 
+            author: "J.R.R. Tolkien", 
+            year: 1954, 
+            pages: 1170 
+        });
+        // console.log("book.id", book.id);
+        book.save((err, book) => {
+            // console.log("book", book);
+
+            chai.request(server)
+                .get('/book/' + book.id)
+                .send(book)
+                .end((err, res) => {
+                    // logResponse(res);
+                    res.should.have.status(200);
+                    res.body.should.be.an('object');
+                    res.body.should.have.property('title');
+                    res.body.should.have.property('author');
+                    res.body.should.have.property('pages');
+                    res.body.should.have.property('year');
+                    res.body.should.have.property('_id').eql(book.id);
+                    done();
+                });
+        });
+            
+      });
   });
+
  /*
   * Test the /PUT/:id route
   */
   describe('/PUT/:id book', () => {
-	  it('it should UPDATE a book given the id', (done) => {
-	  	let book = new Book({title: "The Chronicles of Narnia", author: "C.S. Lewis", year: 1948, pages: 778})
-	  	book.save((err, book) => {
-				chai.request(server)
-			    .put('/book/' + book.id)
-			    .send({title: "The Chronicles of Narnia", author: "C.S. Lewis", year: 1950, pages: 778})
-			    .end((err, res) => {
-				  	res.should.have.status(200);
-				  	res.body.should.be.a('object');
-				  	res.body.should.have.property('message').eql('Book updated!');
-				  	res.body.book.should.have.property('year').eql(1950);
-			      done();
-			    });
-		  });
-	  });
+      it('UPDATE a book given the id', (done) => {
+        let book = new Book({
+            title: "The Chronicles of Narnia", 
+            author: "C.S. Lewis", 
+            year: 1948, 
+            pages: 778
+        });
+        book.save((err, book) => {
+            book.year = 1950;
+            chai.request(server)
+                .put('/book/' + book.id)
+                .send(book)
+                .end((err, res) => {
+                    // logResponse(res);
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('message').eql('Book updated!');
+                    res.body.book.should.have.property('year').eql(book.year);
+                    done();
+                });
+          });
+      });
   });
+
  /*
   * Test the /DELETE/:id route
   */
   describe('/DELETE/:id book', () => {
-	  it('it should DELETE a book given the id', (done) => {
-	  	let book = new Book({title: "The Chronicles of Narnia", author: "C.S. Lewis", year: 1948, pages: 778})
-	  	book.save((err, book) => {
-				chai.request(server)
-			    .delete('/book/' + book.id)
-			    .end((err, res) => {
-				  	res.should.have.status(200);
-				  	res.body.should.be.a('object');
-				  	res.body.should.have.property('message').eql('Book successfully deleted!');
-				  	res.body.result.should.have.property('ok').eql(1);
-				  	res.body.result.should.have.property('n').eql(1);
-			      done();
-			    });
-		  });
-	  });
+      it('DELETE a book given the id', (done) => {
+        let book = new Book({
+            title: "The Chronicles of Narnia", 
+            author: "C.S. Lewis", 
+            year: 1948, 
+            pages: 
+            778
+        });
+        book.save((err, book) => {
+            chai.request(server)
+                .delete('/book/' + book.id)
+                .end((err, res) => {
+                    // logResponse(res);
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('message').eql('Book successfully deleted!');
+                    res.body.result.should.have.property('ok').eql(1);
+                    res.body.result.should.have.property('n').eql(1);
+                    done();
+                });
+          });
+      });
   });
+
 });
   
